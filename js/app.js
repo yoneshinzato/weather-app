07e85b0dc0704dd846756f3d85a2c805e0f19e6d
yoneshinzato <chibariyo@gmail.com>
2021-02-10 00:43:41 -0300

    finishing project

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
 const cityForm = document.querySelector('form')
 const card = document.querySelector('.card')
 const details = document.querySelector(".details")
+const time = document.querySelector('.time')
+const icon = document.querySelector('.icon img')
 
 const updateUI = (data) => {
 
     const cityDets = data.cityDetails
     const weather = data.weather
+    
+    //destructure properties
+    //const { cityDets, weather } = data
 
     //update details template
     details.innerHTML = `
@@ -16,6 +21,23 @@ const updateUI = (data) => {
         <span>&deg;C</span>
     </div>
     `
+
+    //update the night-day and icon images
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`
+    icon.setAttribute('src', iconSrc)
+ 
+
+    let timeSrc = null
+
+    weather.IsDayTime ? timeSrc = 'img/day.svg' : timeSrc = 'img/night.svg'
+    
+    // if(weather.IsDayTime){
+    //     timeSrc = 'img/day.svg'
+    // } else {
+    //     timeSrc = 'img/night.svg'
+    // }
+
+    time.setAttribute('src', timeSrc)
 
     //remove the d-none class if present
     if(card.classList.contains('d-none')){
